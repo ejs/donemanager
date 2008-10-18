41052ff8ca5e490438d3f75b00b6a8907681d29c
Edward Saxton <user@eeepc-edwardsax.65>
2008-10-18 14:59:38 +0100

    Check if store directory exists and if it doesn't create it.

--- donemanager.py
+++ donemanager.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
+from __future__ import with_statement
 import time
 import sys
+import os, os.path
 
+BASEDIR = os.path.expanduser('~/.donemanager')
+if not os.path.exists(BASEDIR):
+   os.mkdir(BASEDIR)
 
 def log(message, at):
     print time.asctime(at), message
