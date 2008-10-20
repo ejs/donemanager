21819e9d7911bb738ce5b1106bd599539bc3e334
Edward Saxton <user@eeepc-edwardsax.65>
2008-10-20 02:18:03 +0100

    start using files, one for each day auto named.

--- donemanager.py
+++ donemanager.py
@@ -10,12 +10,20 @@ if not os.path.exists(BASEDIR):
    os.mkdir(BASEDIR)
 
 
-def log(message, at):
-    print time.asctime(at), message
+def log(message, fn):
+    with open(fn, 'a') as sink:
+        sink.write('%s %s\n'%(time.ctime(), message))
 
+
+def parze(fn):
+    with open(fn) as source:
+        for l in source:
+            print l.strip()
+    
 
 if __name__ == '__main__':
-    if len(sys.argv) < 1:
-        print "Please enter a message."
+    todaysfile = time.strftime(BASEDIR+'/%Y%m%d.txt')
+    if len(sys.argv) < 2:
+        parze(todaysfile)
     else:
-        log(sys.argv[1], time.localtime())
+        log(sys.argv[1], todaysfile)
