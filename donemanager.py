730568a58e322967cf1ab284b546671cf864d485
Edward Saxton <user@eeepc-edwardsax.65>
2008-10-22 12:47:36 +0100

    Task grouping to show total time that day on each task.

--- donemanager.py
+++ donemanager.py
@@ -23,8 +23,16 @@ if __name__ == '__main__':
        os.mkdir(BASEDIR)
     todaysfile = time.strftime(BASEDIR+'/%Y%m%d.txt')
     if len(sys.argv) < 2:
+        totals = {}
+        last = None
         for t, m in parze(todaysfile):
             print t, m
+            if last:
+                totals[m] = totals.get(m, 0) + time.mktime(t) - time.mktime(last)
+            last = t
+        for task in sorted(totals, key=(lambda k:totals[k])):
+            tt = int(totals[task]/60)
+            print task, "%2i:%02i"%(tt/60, tt%60)
         age = int(time.time() - time.mktime(t))/60
         print "Time since last action %2i hours %2i minutes"%(age/60, age%60)
     else:
