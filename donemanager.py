f4d1d95698bd162fef2e6f1d72b75d73054683ea
Edward Saxton <user@eeepc-edwardsax.65>
2008-10-23 14:05:29 +0100

    Bug fix duplicate message code. Split printing out into a seperate function.

--- donemanager.py
+++ donemanager.py
@@ -12,12 +12,15 @@ def log(message, fn):
 
 def parze(fn):
     with open(fn) as source:
-        lm = None
+        lt, lm = None, None
         for l in source:
             t, m = l[:24].strip(), l[24:].strip()
-            if m != lm:
-                yield time.strptime(t), m
+            if lm and m != lm:
+                yield time.strptime(lt), lm
             lm = m
+            lt = t
+        if lm and lt:
+            yield time.strptime(lt), lm
 
 
 def groupeddisplay(log):
@@ -32,26 +35,30 @@ def groupeddisplay(log):
         yield task, tt
 
 
+def basicdisplay(fn):
+    log = [(t, m) for t, m in parze(fn)]
+    validtime = 0
+    for t, m in log:
+        yield "%s\t%s"%(time.ctime(time.mktime(t)), m)
+    yield ''
+    for task, tm in groupeddisplay(log):
+        yield "%s\t%2i:%02i"%(task, tm/60, tm%60)
+        if not task.endswith('**'):
+            validtime += tm
+    mostrecent = max(i[0] for i in log)
+    age = int(time.time() - time.mktime(mostrecent))/60
+    yield "Time since last action %2i hours %2i minutes"%(age/60, age%60)
+    yield "Usefull time today     %2i hours %2i minutes"%(validtime/60, validtime%60)
+    togo = 7*60 - validtime
+    yield "You should still work  %2i hours %2i minutes"%(togo/60, togo%60)
+
 if __name__ == '__main__':
     BASEDIR = os.path.expanduser('~/.donemanager')
     if not os.path.exists(BASEDIR):
        os.mkdir(BASEDIR)
     todaysfile = time.strftime(BASEDIR+'/%Y%m%d.txt')
     if len(sys.argv) < 2:
-        log = [(t, m) for t, m in parze(todaysfile)]
-        validtime = 0
-        for line in log:
-            print "%s\t%s"%(time.ctime(time.mktime(t)), m)
-        print
-        for task, tm in groupeddisplay(log):
-            print "%s\t%2i:%02i"%(task, tm/60, tm%60)
-            if not task.endswith('**'):
-                validtime += tm
-        mostrecent = max(i[0] for i in log)
-        age = int(time.time() - time.mktime(mostrecent))/60
-        print "Time since last action %2i hours %2i minutes"%(age/60, age%60)
-        print "Usefull time today     %2i hours %2i minutes"%(validtime/60, validtime%60)
-        togo = 7*60 - validtime
-        print "You should still work  %2i hours %2i minutes"%(togo/60, togo%60)
+        for line in basicdisplay(todaysfile):
+            print line
     else:
         log(sys.argv[1], todaysfile)
