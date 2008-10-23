342643a1db8014599c16cdde6df70fec63debe81
Edward Saxton <user@eeepc-edwardsax.65>
2008-10-23 13:33:32 +0100

    Start to split display sections out into functions.

--- donemanager.py
+++ donemanager.py
@@ -17,23 +17,34 @@ def parze(fn):
             yield time.strptime(t), d
     
 
+def timedisplay(fn):
+    for t, m in parze(fn):
+        yield "%s\t%s"%(time.ctime(time.mktime(t)), m)
+
+
+def groupeddisplay(fn):
+    totals = {}
+    last = None
+    for t, m in parze(fn):
+        if last:
+            totals[m] = totals.get(m, 0) + time.mktime(t) - time.mktime(last)
+        last = t
+    for task in sorted(totals, key=(lambda k:totals[k])):
+        tt = int(totals[task]/60)
+        yield task, "%2i:%02i"%(tt/60, tt%60)
+    age = int(time.time() - time.mktime(t))/60
+    yield "Time since last action %2i hours %2i minutes"%(age/60, age%60)
+
+
 if __name__ == '__main__':
     BASEDIR = os.path.expanduser('~/.donemanager')
     if not os.path.exists(BASEDIR):
        os.mkdir(BASEDIR)
     todaysfile = time.strftime(BASEDIR+'/%Y%m%d.txt')
     if len(sys.argv) < 2:
-        totals = {}
-        last = None
-        for t, m in parze(todaysfile):
-            print t, m
-            if last:
-                totals[m] = totals.get(m, 0) + time.mktime(t) - time.mktime(last)
-            last = t
-        for task in sorted(totals, key=(lambda k:totals[k])):
-            tt = int(totals[task]/60)
-            print task, "%2i:%02i"%(tt/60, tt%60)
-        age = int(time.time() - time.mktime(t))/60
-        print "Time since last action %2i hours %2i minutes"%(age/60, age%60)
+        for line in timedisplay(todaysfile):
+            print line
+        for line in groupeddisplay(todaysfile):
+            print line
     else:
         log(sys.argv[1], todaysfile)
