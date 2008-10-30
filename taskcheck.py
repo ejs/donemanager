0dfddf1278213f4b30a7f372772a638ed3152f1a
Edward Saxton <user@eeepc-edwardsax.65>
2008-10-30 18:35:08 +0000

    Calculate the number of valid days in the period.

--- taskcheck.py
+++ taskcheck.py
@@ -20,10 +20,17 @@ def summery(basedir, period):
     validtime = sum(actions[task] for task in actions if not task.endswith('**'))
 
 
+def agedfile(basedir, age):
+    date = datetime.datetime.now()-datetime.timedelta(days=age, hours=6)
+    fn = date.strftime(basedir+'/%Y%m%d.txt')
+    return os.path.exists(fn)
+
+
 def aim(basedir, period):
-    pass
+    return sum(1 for i in range(period) if agedfile(basedir, i))
     # load from file formated task name\tdaylow-high\tweeklow-high\tmonthlow-high
     # chose period to check over depending on number of days 'active'
+    # simplify start by only useing format 'task name\tdaylow-high' and multiplying by active days.
     # return dict of clean -> (low, high)
 
 if __name__ == '__main__':
@@ -31,10 +38,13 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1].startswith('-'):
         if sys.argv[1] == '-s':
             log = summery(basedir, 1)
+            print aim(basedir, 1)
         elif sys.argv[1] == '-w':
             log = summery(basedir, 7)
+            print aim(basedir, 7)
         elif sys.argv[1] == '-m':
             log = summery(basedir, 7*4)
+            print aim(basedir, 7*4)
         else:
             print "Defaulting to single day check, use flags for other periods."
             print
