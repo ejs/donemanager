3be589a3c401fd7342a0caae5f033ec9e1c3e234
Edward Saxton <user@eeepc-edwardsax.65>
2008-12-02 15:10:36 +0000

    Switch task checking off by default.

--- donemanager.py
+++ donemanager.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     parser.add_option("-c", action="store_true", dest="chrono", default=False)
     parser.add_option("-g", action="store_true", dest="grouped", default=False)
     parser.add_option("-s", action="store_false", dest="summary", default=True)
-    parser.add_option("-t", action="store_true", dest="tasks", default=True)
+    parser.add_option("-t", action="store_true", dest="tasks", default=False)
     options, args = parser.parse_args()
     if args:
         actor.exposed_log(" ".join(args))
